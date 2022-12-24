975aca1f65f22e1425fdc339a980c937c9d3de55
Zagidullin-Bulat <120476991+Zagidullin-Bulat@users.noreply.github.com>
2022-12-24 21:27:50 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе, которые являются делителями этого числа.
+
+Console.WriteLine ("Введите число:");
+int A = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (A > 0)
+    {
+    int num = A % 10;
+    A = A / 10;
+    sum = sum + num;
+    }
+Console.WriteLine("Сумма всех цифр в числе равна: " + sum);
